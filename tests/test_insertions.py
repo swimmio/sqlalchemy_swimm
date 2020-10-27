e3840f0454737e94c3923c12e4b2e5d1b7d740fb
DivoSwimm <divo@swimm.io>
2020-10-27 15:44:35 +0200

    Add code for unit about inserting data using the SQLAlchemy ORM. Also refactor `test_insertions` to support both test cases.

--- tests/test_insertions.py
+++ tests/test_insertions.py
@@ -1,7 +1,10 @@
 import pytest
 import sqlalchemy as db
 from sqlalchemy.engine import reflection
-from src import insertions
+from src import insertions, insertions_orm
+
+MODE_CORE = 'core'
+MODE_ORM = 'orm'
 
 
 @pytest.fixture(scope='module')
@@ -19,9 +22,16 @@ def db_meta() -> db.MetaData:
     return db.MetaData()
 
 
-def test_insertions(db_engine: db.engine.Engine, db_meta: db.MetaData, db_inspector: reflection.Inspector) -> None:
-    insertions.data_insertions()
+@pytest.mark.parametrize('mode', [MODE_CORE, MODE_ORM])
+def test_insertions(mode: str, db_engine: db.engine.Engine, db_meta: db.MetaData) -> None:
     db_meta.reflect(bind=db_engine)
+    truncate_tables(db_engine, db_meta)
+    if mode == MODE_CORE:
+        insertions.data_insertions()
+        pass
+    elif mode == MODE_ORM:
+        insertions_orm.data_insertions_orm()
+        pass
     users = db_meta.tables['users']
     hobbies = db_meta.tables['hobbies']
     with db_engine.connect() as connection:
@@ -34,6 +44,14 @@ def test_insertions(db_engine: db.engine.Engine, db_meta: db.MetaData, db_inspec
     homer_result = results[0]
     lisa_result = results[1]
     assert homer_result['first_name'] == 'Homer'
+    assert homer_result['last_name'] == 'Simpson'
     assert homer_result['hobby'] == 'Beer'
     assert lisa_result['first_name'] == 'Lisa'
+    assert lisa_result['last_name'] == 'Simpson'
     assert lisa_result['hobby'] == 'Saxophone'
+
+
+def truncate_tables(db_engine: db.engine.Engine, db_meta: db.MetaData) -> None:
+    with db_engine.connect() as connection:
+        for table in db_meta.sorted_tables:
+            connection.execute(table.delete())
