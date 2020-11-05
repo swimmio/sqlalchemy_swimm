79778f68669c3e39703359fb8e8e6e93d89ff9ec
Divo Kaplan <divo@swimm.io>
2020-11-05 11:44:11 +0200

    Hardcode test results for `profitable_music_genre`.

--- tests/test_profitable_music_genre.py
+++ tests/test_profitable_music_genre.py
@@ -1,38 +1,35 @@
-import pytest
-import sqlalchemy as db
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.sql import func
+from decimal import Decimal
+
 from src import profitable_music_genre
-from utils import rockemsocks
+
+EXPECTED_RESULT = [
+    (Decimal('826.65'), 'Rock'),
+    (Decimal('382.14'), 'Latin'),
+    (Decimal('261.36'), 'Metal'),
+    (Decimal('241.56'), 'Alternative & Punk'),
+    (Decimal('93.53'), 'TV Shows'),
+    (Decimal('79.20'), 'Jazz'),
+    (Decimal('60.39'), 'Blues'),
+    (Decimal('57.71'), 'Drama'),
+    (Decimal('40.59'), 'R&B/Soul'),
+    (Decimal('40.59'), 'Classical'),
+    (Decimal('39.80'), 'Sci Fi & Fantasy'),
+    (Decimal('29.70'), 'Reggae'),
+    (Decimal('27.72'), 'Pop'),
+    (Decimal('19.80'), 'Soundtrack'),
+    (Decimal('17.91'), 'Comedy'),
+    (Decimal('16.83'), 'Hip Hop/Rap'),
+    (Decimal('14.85'), 'Bossa Nova'),
+    (Decimal('13.86'), 'Alternative'),
+    (Decimal('12.87'), 'World'),
+    (Decimal('11.94'), 'Science Fiction'),
+    (Decimal('11.88'), 'Heavy Metal'),
+    (Decimal('11.88'), 'Electronica/Dance'),
+    (Decimal('9.90'), 'Easy Listening'),
+    (Decimal('5.94'), 'Rock And Roll'),
+]
 
 
-@pytest.fixture(scope='module')
-def db_engine() -> db.engine.Engine:
-    return db.create_engine(rockemsocks.ROCKEMSOCKSDB_CONNECTION_STRING)
-
-
-@pytest.fixture(scope='module')
-def db_session(db_engine: db.engine.Engine) -> db.orm.session.Session:
-    return sessionmaker(bind=db_engine)
-
-
-def test_most_profitable_music_genre(db_session: db.orm.session.Session) -> None:
+def test_most_profitable_music_genre() -> None:
     tested_result = [tuple(row) for row in profitable_music_genre.most_profitable_music_genre()]
-    session = db_session()
-    expected_result = [
-        tuple(row)
-        for row in (
-            session.query(
-                func.sum(rockemsocks.Tracks.UnitPrice).label('total_invoice_profit'),
-                rockemsocks.Genres.Name.label('genre_name'),
-            )
-            .join(rockemsocks.Genres)
-            .join(rockemsocks.InvoiceItem)
-            .group_by(rockemsocks.Genres.GenreId)
-            .order_by(db.desc('total_invoice_profit'))
-            .all()
-        )
-    ]
-    assert len(tested_result) == len(expected_result)
-    for tested, expected in zip(tested_result, expected_result):
-        assert tested == expected
+    assert tested_result == EXPECTED_RESULT
